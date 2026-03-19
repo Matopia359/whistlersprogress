b9db1aca8c074cc8e79ab624cce09b4466448e28
Matopia359 <kopdoge@gmail.com>
2026-03-19 17:21:39 -0300

    comentei no código todo, debuguei o desenove, organizei melhor o código e fiz a EDEUCF rule

--- numbertoextense/exe.py
+++ numbertoextense/exe.py
@@ -4,16 +4,24 @@ import time
 #EXU = extenso unidade
 #DEU = dezenas e unidades agrupados
 #cf = centena final
-print("os números devem ser feitos com 0, por exemplo: 021 ao invés de 21")
+# df = dezena final
+#uf = unidade final
+#FN = final number
+# criador do numero final | cria o numero final
+# cemrule | alterna entre cem e cento
+#eleven rule | faz os numeros entre 10 e 20 (desenove, dezoito etc)
+# E RULE DO DEU | TIRA OS "E" da dezena e unidade juntos 
+# EDEUCF RULE | é E RULE para o DEU e o CF
+print("os números devem ser feitos com 0, por exemplo: 021 ao invés de 21") #aviso
 time.sleep(1)
 print("")
 numero = input("escreve o número") #input
 c = numero[-3] #separa centena
 d = numero[-2] #separa dezena
 u = numero[-1] #separa unidade
-replace1 = "0"
+replace1 = "0" #variavel pro nada
 null = ""
-EXC = {
+EXC = { # extenso centenas
     "0": "",
     "1": "Cem",
     "2": "duzentos",
@@ -27,7 +35,7 @@ EXC = {
 }
 cf = EXC[c] #centena final é a centena na EXC
     
-EXD = {
+EXD = { #extenso dezenas
     "0": "",
     "1":"dez",
     "2":"vinte",
@@ -41,7 +49,7 @@ EXD = {
 }
 df = EXD[d] # dezena final é a dezena na EXD
 # cria as unidades
-EXU = {
+EXU = { #extenso unidades
     "0": "",
     "1":"um",
     "2":"dois",
@@ -58,62 +66,66 @@ uf = EXU[u] # unidade final é a unidade no EXU
 DEU = df + " e " + uf
 ####### QUEDA DOS NUMEROS ###########
     
-if c == "1" and d == "0" and u == "0":
-    cf = "cem"
-elif c == "1":
-    cf = "cento"
+if c == "1" and d == "0" and u == "0": # se centena for 1 e dezena e unidade for zero
+    cf = "cem" #vira cem
+elif c == "1": # se c for 1 e os outros nao forem zero
+    cf = "cento" #centena final vira cento
 
 ######### QUEDA DO CEMRULE ############
 
-if d == "0" and u == "0":
-    DEU = DEU.replace(" e ", " ")
+if d == "0" and u == "0": # se dezena e unidade for 0
+    DEU = DEU.replace(" e ", " ") #tira o " e " do unidade e dezena agrupados.
     
-if d == "1" and u != "0":
-    df = "dese"
+if d == "1" and u == "8": # se dezena for 1 e u for 8
+    DEU = "dezoito" # dezena e unidade vira dezoito
+    
+if d == "1" and u == "1": # se dezena e unidade for 1
+    df = "on" # manipula a dezena
+    uf = "ze"  # manipula a unidade
+    
+    DEU = df + uf # tira o e, vira dezena com unidade crus
+    
+if d == "1" and u == "2": # bse dezena for 1 e unidade for 2
+    df = "do" #manipula a dezena
+    uf = "ze" #manipula a unidade
+    DEU = df + uf # tira o e, vira dezena com unidade crus
+    
+if d == "1" and u == "3": # se dezena for 1 e unidade for 3
+    df = "tre" #manipula dezena
+    uf = "ze" #manipula unidade
+    DEU = df + uf #tira o e , vira dezena e unidade crus.
+    
+if d == "1" and u == "4": # se for cartoze
+    df = "carto" #manipula dezena
+    uf = "ze" #manipula unidade
+    DEU = df + uf #tira o " e ", vira dezena e unidade crus
+    
+if d == "1" and u == "5": # se for quinze
+    df = "qui" #manipula dezena
+    uf = "ze" #manipula unidade
+    DEU = df + uf #vira dezena e unidade crus
+    
+######## QUEDA DO ELEVENRULE    ############
 
-if d == "1" and u == "8":
-    df = "dez"
-    DEU = "dezoito"
-    
-if d == "1" and u == "1":
-    df = "on"
-    uf = "ze"  
-    DEU = df + uf
-    
-if d == "1" and u == "2":
-    df = "do"
-    uf = "ze"
-    DEU = df + uf
-    
-if d == "1" and u == "3":
-    df = "tre"
-    uf = "ze"
-    DEU = df + uf
-    
-if d == "1" and u == "4":
-    df = "carto"
-    uf = "ze"
-    DEU = df + uf
-    
-if d == "1" and u == "5":
-    df = "qui"
-    uf = "ze"
-    DEU = df + uf
-########  queda do cemrule    ############
-
-if uf == EXU[replace1]:#se uf for zero
-    DEU = df
+if uf == EXU[replace1]:    #se uf for zero
+    DEU = df        # deu vira dezena crua
 elif df == EXD[replace1]: # Se df for zero
-    DEU = uf
-###### QUEDA DO E RULE DO DEU
+    DEU = uf     # DEU vira unidade crua
+    
+###### QUEDA DO E RULE DO DEU #############
 
 FN = cf + " e " + DEU #cria o número final
 
 ############# QUEDA DO CRIADOR DO NUMERO FINAL#####
-if DEU == null:
-    FN = cf 
-if cf == null:
-    FN = DEU
+
+if DEU == null: # se Dezena e unidade for nada
+    FN = cf  # final number vira centena crua
+    
+if cf == null: # SE CENTENA FINAL FOR NADA
+    FN = DEU # FINAL NUMBER VIRA Dezena e unidade
+
+######### QUEDA DO EDEUCF RULE ######### 
+
 if FN == "dez e nove":
     FN = "desenove"
 print(FN)
